48d20bad4b82440d09dd604f9c23e1272a076f52
shivagh1372 <shivagh1372@gmail.com>
2024-06-26 11:38:00 +0330

    script1 is updated

--- script1.py
+++ script1.py
@@ -1,5 +1,5 @@
 import time
-from variables import XPath
+from variables import Variables
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,22 +13,22 @@ driver = webdriver.Chrome()
 # Open a website and maximize
 driver.maximize_window()
 driver.get(HostName.host)
-driver.find_element('class name', XPath.x1).click()
-driver.find_element('xpath', XPath.x2).click()
-elem = driver.find_element('id', XPath.x3)
+driver.find_element('class name', Variables.menu).click()
+driver.find_element('xpath', Variables.menu_item).click()
+elem = driver.find_element('id', Variables.special_word)
 driver.execute_script("arguments[0].scrollIntoView();", elem)
 
 assert elem.text == 'موارد خاص'
 
 # Scroll to the top of the page
 driver.execute_script("window.scrollTo(0, 0);")
-driver.find_element(By.CLASS_NAME, XPath.x4).click()
-driver.find_element(By.ID, XPath.x5).click()
-driver.find_element(By.ID, XPath.x5).send_keys("پژو ۲۰۶")
+driver.find_element(By.CLASS_NAME, Variables.search_icon).click()
+driver.find_element(By.ID, Variables.search_input).click()
+driver.find_element(By.ID, Variables.search_input).send_keys("پژو ۲۰۶")
 
 # Wait for the search result to be present and clickable, then click it
 search_result = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.CSS_SELECTOR, XPath.x6))
+    EC.element_to_be_clickable((By.CSS_SELECTOR, Variables.result_one))
 )
 search_result.click()
 
