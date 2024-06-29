3f57aad2c9d85822a58b045ba351fac618100402
shivagh1372 <shivagh1372@gmail.com>
2024-06-29 15:54:43 +0330

    screen shot function is added

--- script1.py
+++ script1.py
@@ -1,8 +1,8 @@
 import os
 import time
+import sys
 from variables import Variables
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -15,7 +15,9 @@ class CarAdsTest(unittest.TestCase):
         # Initialize the Chrome driver
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
-        pass
+        self.screenshot_dir = 'reports/screenshots'
+        if not os.path.exists(self.screenshot_dir):
+            os.makedirs(self.screenshot_dir)
 
     def test_search_car_ads(self):
         driver = self.driver
@@ -28,6 +30,7 @@ class CarAdsTest(unittest.TestCase):
         # Scroll down to find special word
         elem = driver.find_element(By.ID, Variables.special_word)
         driver.execute_script("arguments[0].scrollIntoView();", elem)
+        time.sleep(3)
         self.assertEqual(elem.text, 'موارد خاص')
 
         # Scroll to the top of the page
@@ -35,8 +38,11 @@ class CarAdsTest(unittest.TestCase):
 
         # Search on search bar and click on first item to show brand ads list
         driver.find_element(By.CLASS_NAME, Variables.search_icon).click()
-        driver.find_element(By.ID, Variables.search_input).click()
-        driver.find_element(By.ID, Variables.search_input).send_keys("پژو ۲۰۶")
+        search_bar = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, Variables.search_input))
+        )
+        search_bar.click()
+        search_bar.send_keys("پژو ۲۰۶")
 
         # Wait for the search result to be present and clickable, then click it
         search_result = WebDriverWait(driver, 10).until(
@@ -50,9 +56,21 @@ class CarAdsTest(unittest.TestCase):
         pass
 
     def tearDown(self):
+        if sys.exc_info()[0] is not None:
+            # Take a screenshot if the test case fails
+            screenshot_name = f'TestResults___main__.CarAdsTest_{time.strftime("%Y-%m-%d_%H-%M-%S")}.png'
+            screenshot_path = os.path.join(self.screenshot_dir, screenshot_name)
+            self.driver.save_screenshot(screenshot_path)
+            print(f'Screenshot saved at: {screenshot_path}')
+
+        else:
+            # Take a screenshot regardless of the test case result
+            screenshot_name = f'TestResults___main__.CarAdsTest_{time.strftime("%Y-%m-%d_%H-%M-%S")}.png'
+            screenshot_path = os.path.join(self.screenshot_dir, screenshot_name)
+            self.driver.save_screenshot(screenshot_path)
+            print(f'Screenshot saved at: {screenshot_path}')
         # Close the browser
         self.driver.quit()
-        pass
 
 
 if __name__ == '__main__':
